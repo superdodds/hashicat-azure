1c1884a500fcc438f8795156dddbb05dc915000e
superdodds <125084525+superdodds@users.noreply.github.com>
2023-02-10 15:14:29 -0600

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,10 +1,6 @@
-variable "resource_group_name" {
-  description = "Name of the Resource Group for new Vnets"
-
-}
 module "network" {
   source  = "app.terraform.io/dodds-htf-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
